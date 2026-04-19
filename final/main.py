501fc6d28f8238d1ed86a95de9b8d2a6653a064d
Dominika Szymczak <155068293+domiwskakuj@users.noreply.github.com>
2026-04-19 19:02:10 +0200

    add queue to main

--- final/main.py
+++ final/main.py
@@ -3,11 +3,14 @@ from BRVBluetoothServer import BRVBluetoothServer
 from BRVDataService import BRVDataService
 import asyncio
 from threading import Thread
+from Queue import Queue
 import time
 import sys
 
 async def waiter():
     await asyncio.sleep(1)
+
+queue = Queue(maxsize=0)
 
 service = BRVDataService()
 
@@ -15,9 +18,9 @@ filename = None
 if len(sys.argv) > 1:
     filename = sys.argv[1]
 
-serial_thread = Thread(target=BRVSerialConnection, args=(service, filename))
+serial_thread = Thread(target=BRVSerialConnection, args=(service, filename, queue))
 serial_thread.daemon = True
-bluetooth_thread = Thread(target=BRVBluetoothServer, args = (service, True))
+bluetooth_thread = Thread(target=BRVBluetoothServer, args = (service, queue))
 bluetooth_thread.daemon = True
 
 print("BRV: starting threads")
